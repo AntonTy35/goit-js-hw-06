0854b7a350d7ba441eeedd1c8e19d297b835b073
AntonTy35 <antony.tislenko@gmail.com>
2023-05-25 19:06:15 +0300

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ulEl = document.querySelector("#ingredients");
+
+ingredients.forEach((element) => {
+  const liEl = document.createElement("li");
+  liEl.textContent = `${element}`;
+  liEl.classList.add("item");
+  ulEl.appendChild(liEl);
+});
