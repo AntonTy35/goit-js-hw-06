0d53e608b8fb02e13d62078fec964f70231358a9
AntonTy35 <antony.tislenko@gmail.com>
2023-06-04 00:24:39 +0300

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  const { email, password } = event.currentTarget.elements;
+
+  const data = {
+    email: email.value,
+    password: password.value,
+  };
+
+  if (email.value === "" || password.value === "") {
+    alert("Bсі поля повинні бути заповнені!");
+  }
+  console.log(data);
+  formEl.reset();
+}
