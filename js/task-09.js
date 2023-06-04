5cf5b9eb2704eb9dc9b2c523db83a684eb66f7c5
AntonTy35 <antony.tislenko@gmail.com>
2023-06-05 00:37:51 +0300

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector("body");
+const spanEl = document.querySelector(".color");
+const btnChangeColor = document.querySelector(".change-color");
+
+btnChangeColor.addEventListener("click", onClick);
+
+function onClick() {
+  bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+  spanEl.textContent = `${bodyEl.style.backgroundColor}`;
+}
