5a3d4553b92a76a40cae39c65b186c01dad72e40
AntonTy35 <antony.tislenko@gmail.com>
2023-06-06 01:07:07 +0300

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -3,6 +3,30 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const controlsEl = document.querySelector("#controls");
+const createBtn = controlsEl.children[1];
+const destroyBtn = controlsEl.children[2];
+const inputEl = document.querySelector('[type="number"]');
+const boxesEl = document.querySelector("#boxes");
 
-// function createBoxes(amount);
-// function destroyBoxes();
+createBtn.addEventListener("click", createBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  amount = inputEl.valueAsNumber;
+  const arrDivEl = [];
+
+  for (let i = 0; i < amount; i += 1) {
+    const divEl = document.createElement("div");
+    divEl.style.width = `${i * 10 + 30}px`;
+    divEl.style.height = `${i * 10 + 30}px`;
+    divEl.style.backgroundColor = getRandomHexColor();
+    arrDivEl.push(divEl.outerHTML);
+  }
+  boxesEl.insertAdjacentHTML("beforeEnd", arrDivEl.join(""));
+}
+
+function destroyBoxes() {
+  boxesEl.innerHTML = "";
+  inputEl.value = "";
+}
